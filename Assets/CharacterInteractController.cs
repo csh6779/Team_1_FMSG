2cc16f3a07e9074c70d25c5e961e5eb1010b79c4
상현 <91441630+csh6779@users.noreply.github.com>
2025-04-01 15:41:14 +0900

    수정

---
+++ Assets/CharacterInteractController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInteractController : MonoBehaviour
+{
+    CharacterController2D characterController;
+    Rigidbody2D rgbd2d;
+    [SerializeField] float offsetDistance = 1f; //플레이어가 사정거리 
+    [SerializeField] float sizeOfInteractableArea = 1.2f; //플레이어의  도구 사정거리
+    Character character;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController2D>();
+        rgbd2d = GetComponent<Rigidbody2D>();
+        character = GetComponent<Character>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Interact();
+        }
+    }
+
+    private void Interact()
+    {
+        Vector2 position = rgbd2d.position + characterController.lastMotionVector * offsetDistance;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, sizeOfInteractableArea);
+
+        foreach (Collider2D c in colliders)
+        {
+            Interactable hit = c.GetComponent<Interactable>();
+            if (hit != null)
+            {
+                hit.Interact(character);
+                break;
+            }
+        }
+    }
+}
